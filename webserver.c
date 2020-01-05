8f5b4cebc02c8c950033e95cf2c743528de4a9ea
tangjinghang <951972210@qq.com>
2020-01-05 20:16:17 +0800

    add a function called rio_writen

--- webserver.c
+++ webserver.c
@@ -39,3 +39,21 @@ else if(nread==0)
 }
 return (n-nleft);
 }
+ssize_t rio_writen(int fd,void *usrbuf,size_t n)
+{
+size_t nleft=n;
+ssize_t nwritten;
+char *bufp=usrbuf;
+while(nleft>0){
+   if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+        if (errno == EINTR)  
+        nwritten = 0;    
+        else
+        return -1;       
+    }
+    nleft -= nwritten;
+    bufp += nwritten;
+    }
+    return n;
+}
+
