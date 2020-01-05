cc29debaf7834c64670ff26526f7f6e12688d06a
tangjinghang <951972210@qq.com>
2020-01-05 21:25:02 +0800

    add som function declaration

--- webserver.c
+++ webserver.c
@@ -140,3 +140,13 @@ if(listen(listen_sock,LISTENQ)<0)
 return -1;
 return listen_sock;
 }
+typedef struct sockaddr SA;
+void process_trans(int fd);
+void read_requesthdrs(rio_t *rp);
+int is_static(char *uri);
+void parse_static_uri(char *uri, char *filename);
+void parse_dynamic_uri(char *uri, char *filename, char *cgiargs);
+void feed_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void feed_dynamic(int fd, char *filename, char *cgiargs);
+void error_request(int fd, char *cause, char *errnum,char *shortmsg, char *description);
