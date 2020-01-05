4f2bb118bae203299d2ee16891b5689f9651f6d0
tangjinghang <951972210@qq.com>
2020-01-05 20:46:43 +0800

    add a function called rio_readnb

--- webserver.c
+++ webserver.c
@@ -86,3 +86,17 @@ rp->rio_cnt=read(rp->rio_fd,rp->rio_buf,sizeof(rp->rio_buf));
     rp->rio_cnt -= cnt;
     return cnt;
 }
+ssize_t rio_readnb(rio_t *rp,void *usrbuf,size_t n){
+size_t nleft=n;
+ssize_t nread;
+char *bufp=usrbuf;
+ while (nleft > 0) {
+    if ((nread = rio_read(rp, bufp, nleft)) < 0) 
+            return -1; 
+    else if (nread == 0)
+        break;
+    nleft -= nread;
+    bufp += nread;
+    }
+    return (n - nleft); 
+}
