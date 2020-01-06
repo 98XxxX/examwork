bfdf50d06aac02686beaca2830c8f675a56e7dcb
tangjinghang <951972210@qq.com>
2020-01-06 14:29:49 +0800

    create a folder to hold cgi and write a file to test

---
+++ cgi-bin/multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+char *buf,*p;
+char content[4096];
+int a=0,b=0;
+scanf("%d&%d",&a,&b);
+sprintf(content,"You can calculate multiplication here\r\n<p>");
+sprintf(content,"%sThe answer is :%d*%d=%d\r\n<p>",content,a,b,a*b);
+ printf("Content-length: %d\r\n", strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+    exit(0);
+}
