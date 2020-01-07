e9fd8a16476396a6031dfbe84ea1b807364a433d
xiao <1306103393@qq.com>
2020-01-07 22:10:11 +0800

    modify the Get.c file

--- cgi-bin/Get.c
+++ cgi-bin/Get.c
@@ -7,7 +7,7 @@ char content[4096];
 int a=0;
 int b=0;
 scanf("a=%d&b=%d", &a, &b);
-sprintf(content, "This is Post\r\n: ");
+sprintf(content, "This is Get\r\n: ");
 sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, a,b,a+b);
 printf("Content-length: %d\r\n", strlen(content));
 printf("Content-type: text/html\r\n\r\n");
